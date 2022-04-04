098c751faed282a68fd3362f63a4c5d81f035f04
JulianCCY <julianchancy@gmail.com>
2022-04-04 16:05:53 +0300

    fix persistence bug

--- WLTF/Persistence.swift
+++ WLTF/Persistence.swift
@@ -14,8 +14,8 @@ struct PersistenceController {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
         for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            let newItem = Food(context: viewContext)
+            newItem.name = String()
         }
         do {
             try viewContext.save()

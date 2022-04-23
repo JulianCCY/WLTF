c0a74934e261009f1096937c75b6e434b913f19a
Borissss420 <borishoi420@gmail.com>
2022-04-23 15:27:58 +0300

    New fridge, minor problem when navigating

--- WLTF/Views/ClosedFridgeScreen.swift
+++ WLTF/Views/ClosedFridgeScreen.swift
@@ -9,10 +9,48 @@ import SwiftUI
 
 struct ClosedFridgeScreen: View {
     
-    @State var imgName: String = "Closed fridge"
+//    @State var imgName: String = "Closedfridge"
     @State var closedFridge = true
     
     var body: some View {
+        VStack {
+            Text("Name of your fridge")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.leading)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .navigationTitle("")
+                .navigationBarHidden(true)
+            if (closedFridge) {
+                Button(action: {
+                    closedFridge = false
+//                imgName = "Openedfridge"
+                }) {
+                    Image("Closedfridge")
+                        .resizable()
+                        .frame(maxWidth: .infinity, minHeight: 625)
+//                        .padding([.leading, .trailing])
+                        .padding()
+                }
+            } else {
+                NavigationLink(destination: InsideFridgeScreen().onAppear {closedFridge = true}) {
+                    Image("Openedfridge")
+                        .resizable()
+                        .frame(maxWidth: .infinity, minHeight: 625)
+        //                .offset(y: -50)
+        //                .ignoresSafeArea()
+//                        .padding([.leading, .trailing])
+                        .padding()
+        //                .navigationBarHidden(true)
+                }
+//                .simultaneousGesture(TapGesture().onEnded{
+//                    closedFridge = true
+//                })
+            }
+        }
+        
+        
+        
 //        NavigationView {
 //            if (closedFridge) {
 //                Button(action: {
@@ -40,25 +78,30 @@ struct ClosedFridgeScreen: View {
 //                Image(imgName)
 //                    .resizable()
 //            }
-        VStack {
-            Text("Name of your fridge")
-                .font(.title)
-                .fontWeight(.bold)
-                .padding(.leading)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            NavigationLink(destination: InsideFridgeScreen()) {
-                Image("Closed fridge")
-                    .resizable()
-                    .frame(width: .infinity, height: 625)
-        //                .offset(y: -50)
-        //                .ignoresSafeArea()
-                    .padding([.leading, .trailing])
-        //            .navigationBarTitle("Name of your fridge")
-        //                .navigationBarHidden(true)
-            }
-            .navigationTitle("")
-            .navigationBarHidden(true)
-        }
+            
+            
+            
+//        VStack {
+//            Text("Name of your fridge")
+//                .font(.title)
+//                .fontWeight(.bold)
+//                .padding(.leading)
+//                .frame(maxWidth: .infinity, alignment: .leading)
+//            NavigationLink(destination: InsideFridgeScreen()) {
+//                Image("Closedfridge")
+//                    .resizable()
+//                    .frame(maxWidth: .infinity, minHeight: 625)
+//        //                .offset(y: -50)
+//        //                .ignoresSafeArea()
+//                    .padding([.leading, .trailing])
+//        //            .navigationBarTitle("Name of your fridge")
+//        //                .navigationBarHidden(true)
+//            }
+//            .navigationTitle("")
+//            .navigationBarHidden(true)
+//        }
+        
+        
     }
 }
 

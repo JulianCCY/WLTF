47639da2bada50a2c05cd2ee17689f2b13786253
Borissss420 <borishoi420@gmail.com>
2022-04-23 23:25:05 +0300

    Sticky note on fridge

--- WLTF/Views/ClosedFridgeScreen.swift
+++ WLTF/Views/ClosedFridgeScreen.swift
@@ -13,43 +13,44 @@ struct ClosedFridgeScreen: View {
     @State var closedFridge = true
     
     var body: some View {
-        VStack {
-            Text("Name of your fridge")
-                .font(.title)
-                .fontWeight(.bold)
-                .padding([.top, .leading])
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .navigationTitle("")
-                .navigationBarHidden(true)
-            if (closedFridge) {
-                Button(action: {
-                    closedFridge = false
-//                imgName = "Openedfridge"
-                }) {
-                    Image("Closedfridge")
-                        .resizable()
-                        .frame(maxWidth: .infinity, minHeight: 625)
-//                        .padding([.leading, .trailing])
-                        .padding()
-                }
-            } else {
-                NavigationLink(destination: InsideFridgeScreen().onAppear {closedFridge = true}) {
-                    Image("Openedfridge")
-                        .resizable()
-                        .frame(maxWidth: .infinity, minHeight: 625)
-        //                .offset(y: -50)
-        //                .ignoresSafeArea()
-//                        .padding([.leading, .trailing])
-                        .padding()
-        //                .navigationBarHidden(true)
-                }
-//                .simultaneousGesture(TapGesture().onEnded{
-//                    closedFridge = true
-//                })
-            }
-        }
-        
-        
+//        VStack {
+//            Text("Name of your fridge")
+//                .font(.title)
+//                .fontWeight(.bold)
+//                .padding([.top, .leading])
+//                .frame(maxWidth: .infinity, alignment: .leading)
+//                .navigationTitle("")
+//                .navigationBarHidden(true)
+//            if (closedFridge) {
+//                Button(action: {
+//                    closedFridge = false
+////                imgName = "Openedfridge"
+//                }) {
+//                    Image("Closedfridge")
+//                        .resizable()
+//                        .frame(maxWidth: .infinity, minHeight: 625)
+////                        .padding([.leading, .trailing])
+//                        .padding()
+//                }
+//            } else {
+//                NavigationLink(destination: InsideFridgeScreen().onAppear {closedFridge = true}) {
+//                    Image("Openedfridge")
+//                        .resizable()
+//                        .frame(maxWidth: .infinity, minHeight: 625)
+//        //                .offset(y: -50)
+//        //                .ignoresSafeArea()
+////                        .padding([.leading, .trailing])
+//                        .padding()
+////                        .ignoresSafeArea()
+//        //                .navigationBarHidden(true)
+//                }
+////                .simultaneousGesture(TapGesture().onEnded{
+////                    closedFridge = true
+////                })
+//            }
+//        }
+//
+//
         
 //        NavigationView {
 //            if (closedFridge) {
@@ -81,25 +82,64 @@ struct ClosedFridgeScreen: View {
             
             
             
-//        VStack {
-//            Text("Name of your fridge")
-//                .font(.title)
-//                .fontWeight(.bold)
-//                .padding(.leading)
-//                .frame(maxWidth: .infinity, alignment: .leading)
-//            NavigationLink(destination: InsideFridgeScreen()) {
-//                Image("Closedfridge")
-//                    .resizable()
-//                    .frame(maxWidth: .infinity, minHeight: 625)
-//        //                .offset(y: -50)
-//        //                .ignoresSafeArea()
-//                    .padding([.leading, .trailing])
-//        //            .navigationBarTitle("Name of your fridge")
-//        //                .navigationBarHidden(true)
-//            }
-//            .navigationTitle("")
-//            .navigationBarHidden(true)
-//        }
+        VStack {
+            Text("Name of your fridge")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(EdgeInsets(top: 30, leading: 20, bottom: 0, trailing: 0))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .navigationTitle("")
+                .navigationBarHidden(true)
+            NavigationLink(destination: InsideFridgeScreen()) {
+                Image("Closedfridge")
+                    .resizable()
+                    .frame(maxWidth: .infinity, minHeight: 625)
+                    .padding()
+                    .overlay(
+                        VStack {
+                            Spacer()
+                            ZStack {
+                                Button {
+   
+                                } label: {
+                                    Image("Orange note")
+                                        .resizable()
+                                        .frame(width: 100, height: 100)
+                                        .rotationEffect(.degrees(-15))
+                                }
+                            }
+                            .offset(x: -70, y: -80)
+                            
+                            ZStack {
+                                Button {
+                                    
+                                } label: {
+                                    Image("Red note")
+                                        .resizable()
+                                        .frame(width: 100, height: 100)
+                                        .rotationEffect(.degrees(20))
+                                }
+                            }
+                            .offset(x: 70, y: -140)
+                            
+                            ZStack {
+                                Button {
+                                    
+                                } label: {
+                                    Image("Purple note")
+                                        .resizable()
+                                        .frame(width: 100, height: 100)
+                                        .rotationEffect(.degrees(-5))
+                                }
+                            }
+                            .offset(x: -30, y: -135)
+                            
+                        }
+                    )
+            }
+            
+//vstack
+        }
         
         
     }

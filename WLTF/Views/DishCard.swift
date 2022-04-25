7720ec7f65fd0126d8023c76ded28957ce27575c
Borissss420 <borishoi420@gmail.com>
2022-04-25 17:00:05 +0300

    DishCard

--- WLTF/Views/DishCard.swift
+++ WLTF/Views/DishCard.swift
@@ -9,34 +9,18 @@ import SwiftUI
 
 struct DishCard: View {
     
-    var title: String = "Food name"
+    var title: String = "Dish name"
     var image: String = "wok"
-    var color: String = "BackgroundColor"
+    var color: String = "BackhgroundColor"
     
     var body: some View {
-        VStack {
-            Text(title)
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundColor(.black)
-                .padding()
-            
-            Spacer()
-            
-            Image(image)
-                .resizable()
-                .renderingMode(.original)
-//                .scaledToFit()
-                .frame(width: 200, height: 200)
-                .padding(.bottom, 20)
+        
+        ZStack {
+            VStack {
+                Text("\(title)")
+            }
         }
-        .background(
-            Circle()
-                .fill(Color("\(color)"))
-        )
-//        .cornerRadius(20)
-//        .frame(width: 250, height: 400)
-//        .shadow(color: Color.gray.opacity(0.4), radius: 10, x: 10, y: 10)
+        
     }
 }
 

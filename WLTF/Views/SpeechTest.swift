6a78c641eb1b1ee78f4f82dce4ed9a53a6a84e20
JulianCCY <julianchancy@gmail.com>
2022-04-29 18:54:48 +0300

    recording voice works

--- WLTF/Views/SpeechTest.swift
+++ WLTF/Views/SpeechTest.swift
@@ -15,13 +15,38 @@ struct SpeechTest: View {
     private var speechManager = SpeechManager()
     
     var body: some View {
-        Button(action: {}) {
-            Image(systemName: recording ? "stop.fill" : "mic.fill")
-                .font(.system(size: 40))
-                .padding()
-                .cornerRadius(10)
+        NavigationView {
+            Button(action: addItem) {
+                Image(systemName: recording ? "stop.fill" : "mic.fill")
+                    .font(.system(size: 40))
+                    .padding()
+                    .cornerRadius(10)
+            }
+            .foregroundColor(.red)
+        }.onAppear{
+            speechManager.checkPermissions()
         }
-        .foregroundColor(.red)
+        
+    }
+     
+    private func addItem() {
+        if speechManager.isRecording {
+            self.recording = false
+            mic.stopMonitoring()
+            speechManager.stopRecording()
+        } else {
+            self.recording = true
+            mic.startMonitoring()
+            speechManager.start{ (speechText) in
+                guard let text = speechText, !text.isEmpty else {
+                    self.recording = false
+                    return
+                }
+                
+                print(text)
+            }
+        }
+        speechManager.isRecording.toggle()
     }
 }
 
